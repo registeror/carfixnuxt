4caffac6c9937a82241a6559dc9c3fe32a7c969a
registeror <registeror100@gmail.com>
2025-07-24 02:59:58 +0300

    Delete middleware directory

--- middleware/secFetchMiddleware.js
+++
@@ -1,8 +0,0 @@
-const ecFetchMiddleware = (req, res, next) => {
-  // Разрешаем запросы только с заголовком 'Sec-Fetch-Mode: cors' (отправляется браузером при fetch/axios)
-  if (req.headers['sec-fetch-mode'] !== 'cors') {
-    return res.status(403).json({ error: 'Доступ запрещен' });
-  }
-  next();
-};
-export default ecFetchMiddleware;
