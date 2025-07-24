11d2a81b5f125b1bffab4effaf037ca3c4280cf8
registeror <registeror100@gmail.com>
2025-07-24 03:39:16 +0300

    Delete .output/server directory

--- .output/server/chunks/build/ProductImage.png.mjs
+++
@@ -1,4 +0,0 @@
-const _imports_0 = "" + __buildAssetsURL("ProductImage.CWmNufuo.png");
-
-export { _imports_0 as _ };
-//# sourceMappingURL=ProductImage.png.mjs.map
--- .output/server/chunks/build/secFetchMiddleware.mjs
+++
@@ -1,9 +0,0 @@
-const ecFetchMiddleware = (req, res, next) => {
-  if (req.headers["sec-fetch-mode"] !== "cors") {
-    return res.status(403).json({ error: "Доступ запрещен" });
-  }
-  next();
-};
-
-export { ecFetchMiddleware as default };
-//# sourceMappingURL=secFetchMiddleware.mjs.map
